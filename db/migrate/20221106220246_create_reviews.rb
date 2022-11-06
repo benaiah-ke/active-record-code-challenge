77c0398764f89d585ada4b2eae5ddd2ff7d2fe09
benaiah-ke <benaiahpraise@gmail.com>
2022-11-07 01:14:20 +0300

    create reviews table

--- db/migrate/20221106220246_create_reviews.rb
+++ db/migrate/20221106220246_create_reviews.rb
@@ -1,4 +1,16 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
+
   def change
+    
+    create_table reviews do |t|
+
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :star_rating
+      t.string :comment
+    
+    end
+
   end
+
 end
