1d0404ecdf4fb5d088eb51cc8934ad449dd6a10e
benaiah-ke <benaiahpraise@gmail.com>
2022-11-07 04:13:19 +0300

    favorite project find trial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ActiveRecord::Base
 
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+
+        self.products.find { |product|
+            product.reviews.star_rating == self.products.maximum(:star_rating)
+        }
+
+
+    end
     
 end
